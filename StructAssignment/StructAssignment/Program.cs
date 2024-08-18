3f986130a8c0d153ec8bab214b8a09b2aa0568dc
Qaramano <166765054+Qaramano@users.noreply.github.com>
2024-08-18 11:56:51 -0600

    Struct Assignment
    Create a struct called Number and give it the property “Amount” and have it be of data type decimal.
    
    In the Main() method, create an object of data type Number and assign an amount to it.
    
    Print this amount to the console.

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StructAssignment
+{
+        // Define a struct called Number
+        struct Number
+        {
+            // Define a property called Amount of type decimal
+            public decimal Amount { get; set; }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // Create an instance of the Number struct
+                Number myNumber = new Number();
+
+                // Assign a value to the Amount property
+                myNumber.Amount = 123.45m;
+
+                // Print the value of the Amount property to the console
+                Console.WriteLine("The amount is: " + myNumber.Amount);
+
+                // Wait for user input before closing the console window
+                Console.ReadLine();
+            }
+        }
+}
