81734e451bac7b2bd13590e8ee8ab89f6285bc8c
Qaramano <166765054+Qaramano@users.noreply.github.com>
2024-08-17 18:34:46 -0600

    Parsing Enums Assignment
    Create an enum for the days of the week.
    
    Prompt the user to enter the current day of the week.
    
    Assign the value to a variable of that enum data type you just created.
    
    Wrap the above statement in a try/catch block and have it print "Please enter an actual day of the week.” to the console if an error occurs.

---
+++ ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ParsingEnumsAssignment
+
+{
+    // Define an enum for the days of the week
+    enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter the current day of the week
+            Console.WriteLine("Please enter the current day of the week:");
+
+            // Read the user input
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                // Try to parse the user input to the DaysOfWeek enum
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+
+                // Output the parsed day
+                Console.WriteLine($"You entered: {currentDay}");
+            }
+            catch (ArgumentException)
+            {
+                // If parsing fails, print an error message
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+}
+
+
