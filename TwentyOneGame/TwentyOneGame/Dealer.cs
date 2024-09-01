5105a2c79ee4f442a314b816943875117e8577b5
Qaramano <166765054+Qaramano@users.noreply.github.com>
2024-09-01 15:17:43 -0600

    TwentyOneGame/BlackJack
     basic console app utilizng .NET Framework to create a Twenty One card game. It includes methods to generate a dealer, deck, and shuffled cards. Logic is used to create the basic rules of Twenty One

---
+++ TwentyOneGame/TwentyOneGame/Dealer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Casino
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; } //does not inherit from deck class; dealer is not a type of deck
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First()); //Add first item of list
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\CarlyScanlan\Desktop\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
+            Deck.Cards.RemoveAt(0); //Remove first item from deck
+        }
+    }
+}
