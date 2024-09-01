0acd9cf8f3805453bef1ac6aa82af804b4de8be7
Qaramano <166765054+Qaramano@users.noreply.github.com>
2024-08-31 19:56:10 -0600

    Final Assignment
    basic Entity Framework Code-First console application that creates a basic Student database and adds one student.

---
+++ StudentApp/StudentApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace codeFirstEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var schoolContext = new SchoolContext())
+            {
+                var stud = new Student() { ID = 01, StudentName = "Omar Kraman" };
+
+                schoolContext.Students.Add(stud);
+                schoolContext.SaveChanges();
+
+                Console.Write("Entry Saved successfully!");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
